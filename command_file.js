0b7731cfbeac581edc887db5fc52219cbc2f27d0
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-06-29 11:13:00 +0530

    GitHub V3 API integration in progress

--- command_file.js
+++ command_file.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 
 let program = require('commander');
-const { readOriginURL } = require('./lib/readGitRepoURL.js');
+const { readOriginURL, readUpstreamURL } = require('./lib/readGitRepoURL.js');
+const { fileIssue } = require('./lib/gitIssueReport.js');
 
 program
-  .version('0.0.1')
+  .version('0.0.1');
 
 program
   .command('fiss')
   .option('-o, --origin', 'file issue in the master(origin) repo')
   .option('-u, --upstream', 'file issue in the upstream(parent) repo')
-  .action(function (cmd) {
+  .action(async function (cmd) {
     if(cmd.origin) {
-      readOriginURL(process.cwd());
+      const url = await readOriginURL(process.cwd());
+      await fileIssue(url);
+    } else if(cmd.upstream) {
+      const url = await readUpstreamURL(process.cwd());
+      await fileIssue(url);
     }
-  })
+  });
 
 program
-  .parse(process.argv)
+  .parse(process.argv);
