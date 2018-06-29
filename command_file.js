150b0316a34f661b53ea0f28b74065bfb8eb7626
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-06-29 14:52:31 +0530

    files now synced upto 50%

--- command_file.js
+++ command_file.js
@@ -7,17 +7,22 @@ const { fileIssue } = require('./lib/gitIssueReport.js');
 program
   .version('0.0.1');
 
+if(!process.env.githubToken) {
+  console.log('Please set your githubToken = "token" as an environment variable');
+  process.exit();
+}
+
 program
-  .command('fiss')
+  .command('file')
   .option('-o, --origin', 'file issue in the master(origin) repo')
   .option('-u, --upstream', 'file issue in the upstream(parent) repo')
   .action(async function (cmd) {
     if(cmd.origin) {
       const url = await readOriginURL(process.cwd());
-      await fileIssue(url);
+      await fileIssue(url, process.env.githubToken);
     } else if(cmd.upstream) {
       const url = await readUpstreamURL(process.cwd());
-      await fileIssue(url);
+      await fileIssue(url, process.env.githubToken);
     }
   });
 
