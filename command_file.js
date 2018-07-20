9d59bc09c3ba0aa8661a0bc36927331f50c8e509
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-07-20 11:48:42 +0530

    enabling vim as a text editor in the CLI interface 40% completed

--- command_file.js
+++ command_file.js
@@ -18,7 +18,7 @@ program
   });
 
 program
-  .command('file <title> [<body>]')
+  .command('finline <title> [<body>]')
   .option('-o, --origin', 'file issue in the master(origin) repo')
   .option('-u, --upstream', 'file issue in the upstream(parent) repo')
   .action(async function (title, body, cmd) {
@@ -39,6 +39,27 @@ program
         body: body
       });
     }
+  });
+
+program
+  .command('feditor')
+  .action(function (cmd) {
+    const fs = require('fs');
+    fs.writeFileSync('commit_message_file.txt',
+    '\n\n\n#### GITISS #### INSTRUCTIONS Below:\n\n' +
+    '** Type `TITLE:` and then enter the title\n' +
+    '** Type `BODY:` and then enter the body(not compulsory)\n\n' +
+    '** Example: \nTITLE: sample_title \nBODY: body_text\n\n' +
+    '** When done editing press `Esc` and type `:wq`\n');
+
+    const child_process = require('child_process');
+    const vim = child_process.spawn('vim', ['commit_message_file.txt'], { stdio: 'inherit' });
+    vim.on('error', () => {
+      console.log('It seems there is no vim installed \n\nRun: sudo apt install vim\n');
+    })
+    vim.on('close', (code) => {
+      console.log('process exited with code: ' + code);
+    });
   });
 
 program
