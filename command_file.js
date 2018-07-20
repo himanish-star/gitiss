d9ee7594be4df11f971872d6ea871723f5d89cf6
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-07-20 15:47:37 +0530

    enabling vim as a text editor in the CLI interface 100% completed

--- command_file.js
+++ command_file.js
@@ -44,10 +44,18 @@ program
 
 program
   .command('feditor')
+  .option('-o, --origin', 'file issue in the master(origin) repo')
+  .option('-u, --upstream', 'file issue in the upstream(parent) repo')
   .action(function (cmd) {
+    if(!process.env.githubToken) {
+      console.log('Please set your githubToken:\n' + 'gitiss token <token>');
+      process.exit();
+    }
+
     const fs = require('fs');
     fs.writeFileSync(__dirname + '/commit_message_file.txt',
     '\n\n\n#### GITISS #### INSTRUCTIONS Below:\n\n' +
+    '** Press `a` to insert text\n' +
     '** Type `TITLE:` and then enter the title(compulsory)\n' +
     '** Type `BODY:` and then enter the body(not compulsory)\n\n' +
     '** Example: \nTITLE: sample_title \nBODY: body_text\n\n' +
@@ -55,13 +63,32 @@ program
 
     const child_process = require('child_process');
     const vim = child_process.spawn('vim', [__dirname + '/commit_message_file.txt'], { stdio: 'inherit' });
+
     vim.on('error', () => {
       console.log('It seems there is no vim installed \n\nRun: sudo apt install vim\n');
     })
+
     vim.on('close', async (code) => {
-      console.log('process exited with code: ' + code);
+      console.log('write process exited with code: ' + code);
       const data2 = await readData();
-      console.log(data2);
+
+      if(!data2.title || data2.title==='') {
+        console.log('ERROR! your title was empty');
+      } else {
+        if(cmd.origin) {
+          const url = await readOriginURL(process.cwd());
+          await fileIssue(url, process.env.githubToken, {
+            title: data2.title,
+            body: data2.body
+          });
+        } else if(cmd.upstream) {
+          const url = await readUpstreamURL(process.cwd());
+          await fileIssue(url, process.env.githubToken, {
+            title: data2.title,
+            body: data2.body
+          });
+        }
+      }
     });
   });
 
